089f4218b958e63d68f14f73f78ad40d3c4740a6
Stéphane Goetz <onigoetz@onigoetz.ch>
2020-09-29 23:01:23 +0200

    Fix node 10 differences

--- packages/intl-formatters/src/number.test.ts
+++ packages/intl-formatters/src/number.test.ts
@@ -51,7 +51,9 @@ it("Formats currencies", () => {
   expect(currencyFormatter("de", "USD", name)(teslaS)).toEqual(
     "69.900,00 US-Dollar"
   );
-  expect(currencyFormatter("zh", "USD", name)(teslaS)).toEqual("69,900.00美元");
+  
+  // Node 10 has different formatting rules
+  expect(currencyFormatter("zh", "USD", name)(teslaS)).toEqual(process.version.indexOf("v10") === 0 ? "69,900.00\xa0美元":"69,900.00美元");
 
   /*expect(currencyFormatter("en", "USD", { style: "accounting" })(-1)).toEqual(
     "($1.00)"
